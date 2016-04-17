82ed14b5a2db84f0f103b9becc89fa532c35bf44
Khải <hvksmr1996@gmail.com>
2016-04-17 14:18:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+'use strict';
+
+var verbs = require('./verbs.js');
+var nouns = require('./nouns.js');
+
+var getstr = (action) =>
+	`${verbs[action.type]} ${nouns[action.itemType]} ${action.itemPath}`;
+
+module.exports = getstr;
